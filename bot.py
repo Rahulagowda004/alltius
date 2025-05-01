a66f84dd57a3258a6742a40181bdd8c8c299d0f7
rahulagowda277 <rahulgowda277@gmail.com>
2025-05-01 13:28:25 +0530

    experimenting

--- bot.py
+++ bot.py
@@ -13,7 +13,9 @@ llm = ChatGroq("llama3-70b-8192",api_key=os.getenv("GROQ_API_KEY"))
 
 embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
 
-vector_store = FAISS.load_local("faiss_index", embeddings=embedding_model)
+vector_store1 = FAISS.load_local("src/RAG/angleone_data/index.faiss", embeddings=embedding_model)
+
+vector_store2 = FAISS.load_local("src/RAG/pdfs_data/index.faiss", embeddings=embedding_model)
 
 class State(TypedDict):
     question: str
@@ -21,10 +23,13 @@ class State(TypedDict):
     answer: str
 
 # Define application steps
-def retrieve(state: State):
-    retrieved_docs = vector_store.similarity_search(state["question"])
+def angleone(state: State):
+    retrieved_docs = vector_store1.similarity_search(state["question"])
     return {"context": retrieved_docs}
 
+def pdf(state: State):
+    retrieved_docs = vector_store2.similarity_search(state["question"])
+    return {"context": retrieved_docs}
 
 def generate(state: State):
     docs_content = "\n\n".join(doc.page_content for doc in state["context"])
@@ -32,8 +37,9 @@ def generate(state: State):
     response = llm.invoke(messages)
     return {"answer": response.content}
 
-
 # Compile application and test
-graph_builder = StateGraph(State).add_sequence([retrieve, generate])
-graph_builder.add_edge(START, "retrieve")
+graph_builder = StateGraph(State).add_sequence([angleone, generate])
+graph_builder.add_edge(START, "angleone")
+graph_builder.add_edge("angleone", "pdf")
+graph_builder.add_edge("pdf", "generate")
 graph = graph_builder.compile()
